f0cdadf0bc68044e52d603f6f29c635317982381
meriemelaji <meriemelhajio@gmail.com>
2023-09-13 11:48:02 -0700

    fils

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,19 +1,28 @@
-#include <limits.h>
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_sign - print + if n is greater than zero.
+ *             0 if n is zero and - if n is less
+ *             than zero .
+ * @n: takes integer type input for function 
  *
- * Return: Always 0.
+ * Return: 1 if +, 0 if 0 and -1 if -
  */
-int main(void)
+int print_sign(int n)
 {
-	int r;
-
-	r = print_sign(98);
-	if (r < 0)
-		r *= -1;
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
+         if (n > 0)
+	 {
+	         -putchar(43);
+	         return (1);
+		 }
+	          else if (n == 0)
+                 {
+		_putchar(48);
+		return (0);
+	}
+	 else    
+	{
+		_putchar(45);
+	        return (-1);
+        }
+}	 	 
