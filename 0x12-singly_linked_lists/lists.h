c9aa6e186fbc281123b1dd8f3ea5a5d53d2f2c11
meriemelaji <meriemelhaji0@gmail.com>
2023-10-19 05:48:06 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,19 +1,26 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Return: number of elements in h
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-size_t list_len(const list_t *h)
+typedef struct list_s
 {
- size_t n = 0;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
-}
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
